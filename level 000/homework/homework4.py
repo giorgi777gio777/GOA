609e21bc5bb172bd5d21454667463cd48d79071f
giorgi777gio77 <gio.khl.a4@gmail.com>
2025-06-17 13:43:04 +0400

    commit

---
+++ level 000/homework/homework4.py
@@ -0,0 +1,13 @@
+#  4) გაასწორეთ კოდი და მიუწერეთ რატომ იყო ძველი ვერსია არასწორი;
+
+#      ----------------
+#      num1 = 10
+#      ----------------
+#      academy = 'Goal Oriented Academy'
+#      ----------------
+#      name = 'Jumbera'
+#      name2 = 'mayvala'
+#      print(name == name2)
+#      num = 30
+#      Print(num)+20)
+
---
+++ level 000/homework/homework8.py
@@ -0,0 +1,7 @@
+name = input("შეიყვანე სენი სახელი")
+lastname = input('შეიყვანე სენი გვარი')
+height = input(' შეიყვანე შენი სიმაღლე')
+
+print  (name)
+print  (lastname)
+print  (int(height))
