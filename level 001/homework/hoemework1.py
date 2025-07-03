211f5fa6ee00e16a37e5cb55987efc63bc4b7eb2
giorgi777gio77 <gio.khl.a4@gmail.com>
2025-07-03 18:52:25 +0400

    commit

---
+++ level 001/homework/hoemework1.py
@@ -0,0 +1,5 @@
+# 1)შექმენით სამი ცვლადი snake_case ის გამოყენებით მიანიჭეთ ამ ცვლადებსი სასურველი მნიშვნელობები და გამოიტანეთ ტერმინალში
+galaxy_new = 'new_poko_m5'
+lenowo_is_my_computer = 'lenowo'
+satvale_sportuli = 'miketia_am_wams'
+print(galaxy_new,lenowo_is_my_computer,satvale_sportuli)
---
+++ level 001/homework/homework9.py
@@ -0,0 +1,14 @@
+# 9)მომხმარებელს შემოატანინეთ ორი რიცხვი,თქვენი დავალებაა მოახდინოთ ყველა მათემატიკური ოპერაცია ამ რიცხვებზე, +,-./,//,*,==,%,ასევე კომენტარის სახით ახსენით რას აკეთებს % ნიშანი (ვინც აქამდე ჩამოხვედით და წინ რაც იყო ყველაფერი გააკეთეთ
+
+number =int (input('შემოვიტანოტ რიცხვები'))
+number2 =int (input('შემოიტანეთ რიცხვები'))
+print(number + number2)
+print(number2 - number)
+print(number / number2)
+print(number // number2)
+print(number * number2)
+print(number*number2)
+print(number == number2)
+print(number % number2)
+
+# % = ეს ნიშანი გამოხატავს ნაშთს
